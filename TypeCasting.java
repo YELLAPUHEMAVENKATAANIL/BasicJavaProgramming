d627abb571389b5b6e4b687f91c882dac8834aee
YELLAPUHEMAVENKATA <anilyallapu12345@gmail.com>
2025-05-18 12:41:17 +0530

    Type casting (Concept)

---
+++ TypeCasting.java
@@ -0,0 +1,21 @@
+
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+         // this is automatic Casting  int value converted to double
+       int num = 10;
+       double dvalue = num;
+       System.out.println("Double value is "+dvalue); 
+
+
+        // this is manual Casting 
+
+        double dvalue2 = 20.0;
+        int num2 =(int) dvalue2;
+        System.out.println("Integer value is "+num2);
+
+        
+    }
+    
+}
